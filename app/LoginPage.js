39b582d5abe7363959b374fbb12495cea56892d6
YashviDesaii <yashvi.desai999@gmail.com>
2020-11-13 11:13:49 +0530

    initial commit

---
+++ app/LoginPage.js
@@ -0,0 +1,20 @@
+
+'use strict';
+
+import React, {Component, PropTypes} from 'react';
+import Header from './Header';
+import LoginForm from './LoginForm';
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <LoginForm
+          loginCallBacks={this.props.loginCallBacks} />
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
